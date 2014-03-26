3821bfa4656f9748dd9aade06974153fa29ed3cc
Dr. Torge Szczepanek <t.szczepanek@cygnusnetworks.de>
2014-03-26 21:08:03 +0100

    ignore all providers which cannot be loaded due to missing imports

--- pynotifyd/providers/all.py
+++ pynotifyd/providers/all.py
@@ -1,24 +1,59 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pynotifyd.providers.developergarden import ProviderDevelopergarden
-from pynotifyd.providers.sipgate import ProviderSipgate
-from pynotifyd.providers.shell import ProviderShell
-from pynotifyd.providers.mock import ProviderMock
-from pynotifyd.providers.jabber import ProviderJabber
-from pynotifyd.providers.mail import ProviderMail
-from pynotifyd.providers.persistentjabber import ProviderPersistentJabber
+__all__ = []
+provider_drivers = {}
+provider_errors = {}
 
-__all__ = ["ProviderDevelopergarden", "ProviderSipgate", "ProviderShell",
-		"ProviderMock", "ProviderJabber", "ProviderMail"]
+try:
+	from pynotifyd.providers.developergarden import ProviderDevelopergarden
+	__all__.append("ProviderDevelopergarden")
+	provider_drivers["developergarden"] = ProviderDevelopergarden
+except Exception, msg:
+	provider_errors["developergarden"] = msg
+
+try:
+	from pynotifyd.providers.sipgate import ProviderSipgate
+	__all__.append("ProviderSipgate")
+	provider_drivers["sipgate"] = ProviderSipgate
+except Exception, msg:
+	provider_errors["sipgate"] = msg
+
+try:
+	from pynotifyd.providers.shell import ProviderShell
+	__all__.append("ProviderShell")
+	provider_drivers["shell"] = ProviderShell
+except Exception, msg:
+	provider_errors["shell"] = msg
+
+try:
+	from pynotifyd.providers.mock import ProviderMock
+	__all__.append("ProviderMock")
+	provider_drivers["mock"] = ProviderMock
+except:
+	pass
+
+try:
+	from pynotifyd.providers.jabber import ProviderJabber
+	__all__.append("ProviderJabber")
+	provider_drivers["jabber"] = ProviderJabber
+except Exception, msg:
+	provider_errors["jabber"] = msg
+
+try:
+	from pynotifyd.providers.mail import ProviderMail
+	__all__.append("ProviderMail")
+	provider_drivers["mail"] = ProviderMail
+except Exception, msg:
+	provider_errors["mail"] = msg
+
+try:
+	from pynotifyd.providers.persistentjabber import ProviderPersistentJabber
+	__all__.append("ProviderPersistentJabber")
+	provider_drivers["persistentjabber"] = ProviderPersistentJabber
+except Exception, msg:
+	provider_errors["persistentjabber"] = msg
 
 __all__.append("provider_drivers")
-provider_drivers = dict(
-		developergarden=ProviderDevelopergarden,
-		sipgate=ProviderSipgate,
-		shell=ProviderShell,
-		mock=ProviderMock,
-		jabber=ProviderJabber,
-		mail=ProviderMail,
-		persistentjabber=ProviderPersistentJabber)
+
 
