3e84944042bb162c64d5b393a805636d04d41478
Dr. Torge Szczepanek <t.szczepanek@cygnusnetworks.de>
2014-05-20 14:51:28 +0200

    Switch to debhelper 9

--- setup.py
+++ setup.py
@@ -9,17 +9,18 @@ setup(name='pynotifyd',
 	version=__version__,
 	description="Python Notification Daemon",
 	long_description="The daemon allows you to send a message to a contact via jabber, email or sms whatever fits best. It is intended for use with Nagios.",
-	author='Helmut Grohne',
-	author_email='h.grohne@cygnusnetworks.de',
+	author='Helmut Grohne, Torge Szczepanek',
+	author_email='debian@cygnusnetworks.de',
 	maintainer='Torge Szczepanek',
 	maintainer_email='debian@cygnusnetworks.de',
-	license='GNU GPLv3', 
+	license='GNU GPLv3',
 	packages=['pynotifyd', "pynotifyd.providers"],
 	scripts=['pynotifyd_client', 'pynotifydaemon', 'developergarden_ctl'],
 	classifiers=[
 		"Development Status :: 4 - Beta",
 		"Intended Audience :: System Administrators",
 		"Programming Language :: Python",
-		]
+		],
+	install_requires=["configobj", "pyinotify", "setproctitle", "pyxmpp", "gsmsapi", "phonenumbers"],
 	)
 
